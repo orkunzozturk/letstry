ac4496b57623e152b70c1e1096d7f241aaf4a018
orkunzozturk <orkunzozturk@gmail.com>
2022-01-13 17:12:19 +0800

    changed head meta code for google verification

--- src/components/CustomHead.tsx
+++ src/components/CustomHead.tsx
@@ -4,6 +4,6 @@ import { Helmet } from 'react-helmet';
 
 export const CustomHead = () => (
   <Helmet>{
-    <meta name="google-site-verification" content="BGu-WExnWQbhbi0M0B4PhFzTtvF5EUD44vqqJdiGZOY" />
+    <meta name="google-site-verification" content="obcIb3t5hRLbAjwptkzS6J6B4mzDYixkXGg5dI1NmEk" />
     /* Replace with whatever you want */}</Helmet>
 );
